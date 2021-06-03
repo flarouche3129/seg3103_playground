a144c7e0de879a835e5741866599888cef209be1
Félix Larouche <flarouche3129@gmail.com>
2021-06-03 18:06:14 -0400

    Cleaning up stuff to do lab4

--- Lab4/fizzbuzz_java/test/FizzBuzzTest.java
+++ Lab4/fizzbuzz_java/test/FizzBuzzTest.java
@@ -2,10 +2,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
-    
-    @Test
-    void firstTest(){
-        assertTrue(true);
-    }
 
+    
 }
