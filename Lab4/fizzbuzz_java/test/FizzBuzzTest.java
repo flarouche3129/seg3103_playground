eeb00aae123d688c93032e801882218cb79c3966
Félix Larouche <flarouche3129@gmail.com>
2021-06-03 15:37:56 -0400

    Created FizzBuzz.java

---
+++ Lab4/fizzbuzz_java/test/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+    
+    @Test
+    void firstTest(){
+        assertTrue(true);
+    }
+
+}
