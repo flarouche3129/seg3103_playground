c822d439de86366c9b1defc68c83f0e67d908a1c
Emma Dobson Takoff <edobs010@uottawa.ca>
2021-06-06 11:12:13 -0400

    Refactored for Q2.1 - extracted helper method avg

--- Assignement2/lib/grades/calculator.ex
+++ Assignement2/lib/grades/calculator.ex
@@ -1,37 +1,34 @@
 defmodule Grades.Calculator do
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    
+    # Refactored for Question 2.1
+    {avg_homework, avg_labs} = avg(homework, labs)
 
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
-  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+  # Helper method for Question 2.1
+  def avg(homework, labs) do 
     avg_homework =
       if Enum.count(homework) == 0 do
         0
       else
         Enum.sum(homework) / Enum.count(homework)
-      end
-
+    end
     avg_labs =
       if Enum.count(labs) == 0 do
         0
       else
         Enum.sum(labs) / Enum.count(labs)
-      end
+    end
+    {avg_homework, avg_labs}
+  end
+
+  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    
+    # Refactored for Question 2.1
+    {avg_homework, avg_labs} = avg(homework, labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -62,19 +59,9 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    
+    # Refactored for Question 2.1
+    {avg_homework, avg_labs} = avg(homework, labs)
 
     avg_exams = (midterm + final) / 2
 
