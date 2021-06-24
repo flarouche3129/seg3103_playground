42351167e63c306fa8882742745a7c5c502a2b61
Félix Larouche <flarouche3129@gmail.com>
2021-06-24 17:02:15 -0400

    Stubbed Grades.Calculator

--- Lab5/grades/lib/grades.ex
+++ Lab5/grades/lib/grades.ex
@@ -1,4 +1,4 @@
-defmodule Grades do
+defmodule Grades.Calculator do
   @moduledoc """
   Grades keeps the contexts that define your domain
   and business logic.
@@ -6,4 +6,15 @@ defmodule Grades do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  def percentage_grade(marks) do
+    :rand.uniform(100)
+  end
+
+  def letter_grade(marks) do
+    Enum.at(["A+", "A", "A-", "B+", "B", "C+", "C", "D+", "D", "E", "F"], :rand.uniform(10))
+  end
+
+  def numeric_grade(marks) do
+    :rand.uniform(10)
+  end
 end
